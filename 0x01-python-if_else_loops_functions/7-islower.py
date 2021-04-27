f39ca577819ae1839f2ba6e8dbd94584e27adff0
livejo <yoseph.kefale@gmail.com>
2021-04-27 18:34:02 +0300

    Update 7-islower.py

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def islower(c):
-  lower = ord(c)
-  if lower < 97 or lower >122:
-    return true
-  else:
-    return false
+    if ord('a') <= ord(c) <= ord('z'):
+        return True
+    else:
+        return False
