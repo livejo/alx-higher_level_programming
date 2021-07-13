c37f423ead2b9e0a219a349db54500b6c3e309c8
livejo <yoseph.kefale@gmail.com>
2021-07-13 22:23:28 +0300

    Update 2-create_read_user.sql

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,6 +1,5 @@
--- script that creates the database hbtn_0d_2 and the user user_0d_2.
-CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
-DROP USER user_0d_2@localhost;
+-- creates a user and password with select privilages
+CREATE database IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON `hbtn_0d_2`.* TO 'user_0d_2'@'localhost';
 FLUSH PRIVILEGES;
-CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost;
