ab7e2e07c546cf9fa08df2c46ad8a637c0628e1e
livejo <yoseph.kefale@gmail.com>
2021-08-02 20:29:42 +0300

    Update 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
-/*
-prints statements about c, python, and javascript */
-const cStatement = 'C is fun';
-const pStatement = 'Python is cool';
-const jStatement = 'Javascript is amazing';
-console.log(cStatement);
-console.log(pStatement);
-console.log(jStatement);
+const myVar1 = 'C is fun';
+const myVar2 = 'Python is cool';
+const myVar3 = 'JavaScript is amazing';
+console.log(myVar1);
+console.log(myVar2);
+console.log(myVar3);
